ee14245a15fb1d6e0700a7587b4361ec8c4e5546
napiersk <napiersk@isi.edu>
2022-07-01 12:28:01 -0400

    dotenv added

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+
+function createTrainingFiles(triggers){
+// in NLI_PATH folder, create $t.txt
+// in NLU_PATH folder, create $t.txt, NOT$t.txt
+  console.log(triggers);
+  console.log(process.env.NLI_PATH);
+  console.log(process.env.NLU_PATH);
+}
 
 app.get('/', (req, res) => {
   res.send('Agenda-Express API Running.');
@@ -9,10 +18,9 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const triggers=req.header('TRIGGERS');
   console.log('TRIGGERS: ' + triggers);
-// TODO: create directories and files
+  createTrainingFiles(triggers.split(','));
   res.end('SUCCESS');
 });
-
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
