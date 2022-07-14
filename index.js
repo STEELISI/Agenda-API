9514f3913fe56a8659dca7a6a14fe315a6dd6018
napiersk <napiersk@isi.edu>
2022-07-14 18:13:39 -0400

    agenda log

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const path = require('node:path');
 const touch = require('touch');
 const nliPath = process.env.NLI_PATH + '';
 const nluPath = process.env.NLU_PATH + '';
+const agendaPath = process.env.AGENDA_PATH + '';
 const port = Number(process.env.PORT);
 const sslPort = Number(process.env.SSL_PORT);
 const sslKey = process.env.SSL_KEY + '';
@@ -18,14 +19,17 @@ async function doCreateTrainingFiles(triggers){
   console.log(triggers);
   console.log(nliPath);
   console.log(nluPath);
+  console.log(agendaPath);
 
   await makeDir(nliPath);
   await makeDir(nluPath);
+  await makeDir(agendaPath);
 
   for (let t in triggers) {
-    touch.sync(path.join(nliPath, t + '.txt'));
-    touch.sync(path.join(nluPath, t + '.txt'));
-    touch.sync(path.join(nluPath, 'NOT' + t + '.txt'));
+    let tName = triggers[t];
+    touch.sync(path.join(nliPath, tName + '.txt'));
+    touch.sync(path.join(nluPath, tName + '.txt'));
+    touch.sync(path.join(nluPath, 'NOT' + tName + '.txt'));
   }
 }
 
@@ -43,7 +47,14 @@ app.post('/', (req, res) => {
   const triggers=req.header('TRIGGERS');
   console.log('TRIGGERS: ' + triggers);
   createTrainingFiles(triggers.split(','));
-  res.end('SUCCESS');
+
+  const training=req.header('TRAINING');
+  console.log('TRAINING: ' + training);
+  
+  const agenda=req.header('AGENDA');
+  console.log('AGENDA: ' + agenda);
+
+  res.end('{"result":"SUCCESS"}');
 });
 
 app.listen(port, () => {
